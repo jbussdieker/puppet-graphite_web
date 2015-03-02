2f7d60d95622397339785373b2afb6d52ac93551
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-03-01 17:35:34 -0800

    Working test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe 'graphite_web' do
-  it { should have_resource_count(7) }
+  let(:facts) do
+    {
+      :operatingsystem => operatingsystem,
+      :operatingsystemmajrelease => operatingsystemmajrelease
+    }
+  end
+
+  context 'Ubuntu' do
+    let(:operatingsystem) { 'Ubuntu' }
+
+    context '14.04' do
+      let(:operatingsystemmajrelease) { '14.04' }
+      it { should have_resource_count(62) }
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,10 @@ require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 at_exit { RSpec::Puppet::Coverage.report! }
+
+RSpec.configure do |c|
+  c.default_facts = {
+    :kernel          => 'Linux',
+    :concat_basedir  => '/var/lib/puppet/concat',
+  }
+end
